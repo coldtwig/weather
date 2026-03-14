229978f442c88b049dca8df77788cb049dd0cce0
Yuri Chebunin <chebuninyuri@gmail.com>
2026-03-14 10:38:54 +0800

    created weather and ip package

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"demo/weather/geo"
+	"demo/weather/weather"
+	"flag"
+	"fmt"
+)
+
 func main() {
+	city := flag.String("city", "", "Город пользователя")
+	format := flag.Int("format", 1, "Формат вывода погоды")
+
+	flag.Parse()
+
+	geoData, err := geo.GetMyLocation(*city)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	weather, err := weather.GetWeather(*geoData, *format)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Printf("Погода в городе %s сейчас %s", geoData.City, weather)
 }
---
+++ weather/weather.go
@@ -0,0 +1,38 @@
+package weather
+
+import (
+	"demo/weather/geo"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+func GetWeather(geo geo.GeoData, format int) (string, error) {
+	baseUrl, err := url.Parse("https://www.wttr.in/" + geo.City)
+	if err != nil {
+		return "", err
+	}
+
+	params := url.Values{}
+	params.Set("city", geo.City)
+	params.Set("format", fmt.Sprint(format))
+
+	baseUrl.RawQuery = params.Encode()
+
+	resp, err := http.Get(baseUrl.String())
+	if err != nil {
+		return "", err
+	}
+	if (*resp).StatusCode != 200 {
+		return "", errors.New("NOT_200")
+	}
+
+	body, err := io.ReadAll((*resp).Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
